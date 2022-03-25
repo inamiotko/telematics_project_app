43e9fc1e8a4c5aba7015df1f4d03bfacf1400c6a
inamiotko <iganamiotko14@gmail.com>
2022-03-25 15:31:27 +0100

    initialized firebase added some basic settings

--- app/src/main/java/com/example/projekt_telemedycyna/ui/MainActivity.kt
+++ app/src/main/java/com/example/projekt_telemedycyna/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.projekt_telemedycyna
+package com.example.projekt_telemedycyna.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projekt_telemedycyna.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
