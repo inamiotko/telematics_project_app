9d0400608870bb119cd31ab121ee1f52f0ffb8b8
inamiotko <iganamiotko14@gmail.com>
2022-03-25 15:50:35 +0100

    no message

---
+++ app/src/main/java/com/example/telematics_project/TelematicsProjectApplication.kt
@@ -0,0 +1,20 @@
+package com.example.telematics_project
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class TelematicsProjectApplication : Application() {
+    companion object{
+        lateinit var context: TelematicsProjectApplication private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseApp.initializeApp(this)
+    }
+}
